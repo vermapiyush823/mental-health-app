6b644133baa498d8224988b821fc95d0fa96786b
Piyush Verma <piyush.verma1@servicenow.com>
2025-03-21 11:57:26 +0530

    feat: update Overview_single_score component to utilize full width and height

--- src/components/shared/Overview_single_score.tsx
+++ src/components/shared/Overview_single_score.tsx
@@ -9,7 +9,7 @@ const overview_single_score = (
     {catgory, score}: Props
 ) => {
   return (
-    <div className='flex flex-col bg-black/15 p-4 rounded-lg '>
+    <div className='flex flex-col bg-black/15 p-4 rounded-lg w-full h-full'>
         <p className='text-sm text-gray-500 '>{catgory}</p>
         <p className='text-lg font-bold'>
             {
