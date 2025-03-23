bf0720c0459455f75a5c779e2a9acd8f95c2383d
Piyush Verma <piyush.verma1@servicenow.com>
2025-03-23 16:08:53 +0530

    feat: retrieve user_id from cookies and pass to Dashboard_profile component

--- src/app/(route)/support/page.tsx
+++ src/app/(route)/support/page.tsx
@@ -3,11 +3,17 @@ import Dashboard_profile from '@/components/shared/Dashboard_profile'
 import Chatbot from '@/components/shared/Chatbot'
 import SupportNetwork from '@/components/shared/Support_network'
 import Chat_Features from '@/components/shared/Chat_Features'
-const page = () => {
+import { cookies } from 'next/headers'
+
+const page = async() => {
+    const cookieStore = await cookies();
+    const userId = cookieStore.get("user_id")?.value || "";
   return (
     <div className="flex flex-col mt-8 mb-8 gap-y-5 w-full items-center">
       <div className='w-full justify-center items-center hidden sm:flex'>
-        <Dashboard_profile/>
+        <Dashboard_profile  userId={userId}
+
+        />
       </div>
       <div className="w-[95%] grid grid-cols-1 sm:grid-cols-3 gap-5">
           <div className='col-span-1 sm:col-span-2  w-full'>
